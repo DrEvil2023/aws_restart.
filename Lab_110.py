92e187ad996ac5150b588c1ad4af9a2117f73725
DrEvil2023 <126687267+DrEvil2023@users.noreply.github.com>
2023-03-03 23:44:55 +0100

    Add files via upload

---
+++ Lab_110.py
@@ -0,0 +1,25 @@
+# String
+# Task 1
+# print("Python has three numeric types: int, float, and complex")
+
+
+# Int
+# Task 2
+# myValue=1
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Floot
+# Task 3
+# myValue=3.14
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Complex
+# Task 4
+# myValue=5j
+# print(myValue)
+# print(type(myValue))
+# print(str(myValue) + " is of the data type " + str(type(myValue)))
